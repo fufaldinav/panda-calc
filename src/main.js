1206c326a5a0d358e6c077712c9d413036b688a9
Artem Fufaldin <artem.fufaldin@gmail.com>
2023-07-06 05:21:29 +0500

    build: install quasar

--- src/main.js
+++ src/main.js
@@ -1,6 +1,17 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import { Quasar } from 'quasar'
+import quasarLang from 'quasar/lang/ru'
+
+import '@quasar/extras/roboto-font/roboto-font.css'
+import 'quasar/src/css/index.sass'
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App)
+  .use(Quasar, {
+    plugins: {},
+    lang: quasarLang
+  })
+  .mount('#app')
